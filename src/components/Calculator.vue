d16bf205457971493856a068c8fdadcc1af5d747
William Maphanga <william@colonyhq.com>
2018-04-08 11:05:16 +0200

    Added Calculator UI

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,16 +1,76 @@
 <template>
   <div>
-      <div class="row">
-          <div class="col">
-            
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+
+        <form>
+
+          <div class="form-row">
+            <label class="col-sm-8 col-form-label">Which tax year would you like to calculate?</label>
+            <div class="col-sm-4">
+              <select class="form-control form-control-sm">
+                <option v-for="year in years" :key="year">{{ year }}</option>
+              </select>
+            </div>
           </div>
+
+          <div class="form-row">
+            <label class="col-sm-8 col-form-label">What is your total salary before deductions? </label>
+            <div class="col-sm-4">
+              <div class="input-group-prepend">
+                <div class="input-group-text">R</div>
+              <input type="number" class="form-control" id="inlineFormInputGroup" placeholder="Salary">
+              </div>
+            </div>
+          </div>
+
+          <div class="form-row">
+            <label class="col-sm-8 col-form-label">How often do you receive this salary? </label>
+            <div class="col-sm-4">
+              <select class="form-control form-control-sm">
+                <option v-for="period in payPeriods" :key="period">{{ period }}</option>
+              </select>
+            </div>
+          </div>
+
+         <div class="form-row">
+            <label class="col-sm-8 col-form-label">Your age?</label>
+            <div class="col-sm-4">
+              <input type="number" class="form-control" id="inlineFormInputGroup" v-model="age" placeholder="age">
+            </div>
+          </div>
+
+           <div class="form-row">
+            <label class="col-sm-8 col-form-label">Number of medical aid dependent(s):</label>
+            <div class="col-sm-4">
+              <input type="number" class="form-control" id="inlineFormInputGroup" v-model="dependents" placeholder="No. of Dependents">
+            </div>
+          </div>
+  <br>
+           <div class="form-row">
+             <div class="col">
+             <button class="btn btn-primary btn-block">Submit</button>
+             </div>
+           </div>
+
+        </form>
+
+
       </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data (){
+    return {
+      years: [2017, 2018],
+      payPeriods: ['Monthly','Annualy'],
+      age: 18,
+      dependents: 0
+    }
+  }
 }
 </script>
 
